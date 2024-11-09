5d8cceb6773f3b2d26ce590d771d4896b0d83a5c
oltapllana <oltapllana@gmail.com>
2024-11-09 12:23:07 +0100

    small changes

--- src/components/marvel-characters.vue
+++ src/components/marvel-characters.vue
@@ -5,18 +5,20 @@
         <hr>
         <div class="parent ">
             <ul>
-                <li v-for="marvelcharacter in filteredCharacters"  :key="marvelcharacter.id">
-                    <router-link :to="{name: 'marvel-character', params: {id: marvelcharacter.id}}"><img src="../assets/marvel.jpg" alt="">
+                <li v-for="marvelcharacter in filteredCharacters" :key="marvelcharacter.id">
+                    <router-link :to="{ name: 'marvel-character', params: { id: marvelcharacter.id } }"><img
+                            :src="`${marvelcharacter.thumbnail.path}.${marvelcharacter.thumbnail.extension}`"
+                            :alt="marvelcharacter.name" class="character-img" />
                         {{ marvelcharacter.name }}
                         <button>Show more</button>
-                     </router-link>
+                    </router-link>
                 </li>
             </ul>
         </div>
     </div>
 </template>
 <style>
-li a{
+li a {
     color: black;
     font-size: larger;
     display: flex;
@@ -24,17 +26,24 @@ li a{
     flex-direction: column;
     align-items: center;
 }
-li{
+
+li {
     margin-top: 10px;
-} 
-input{
+}
+
+input {
     padding: 15px 30px;
     margin-bottom: 30px;
+}
+.character-img{
+    width: 300px;
+    height: 300px;
+    object-fit: contain;
 }
 </style>
 <script>
 import { public_key } from '@/views/marvelCharacters';
-    export default{
+export default {
     name: "characters-marvel",
     data() {
         return {
@@ -50,63 +59,67 @@ import { public_key } from '@/views/marvelCharacters';
             fetch(`http://gateway.marvel.com/v1/public/characters?apikey=${public_key}`)
                 .then(response => response.json())
                 .then((result) => {
-                console.log(result);
-                result.data.results.forEach((item) => {
-                    console.log(item);
-                    this.marvelCharacters.push(item);
-                });
-            })
+                    console.log(result);
+                    result.data.results.forEach((item) => {
+                        console.log('item', item);
+                        this.marvelCharacters.push(item);
+                    });
+                })
                 .catch((error) => {
-                console.log(error);
-            });
+                    console.log(error);
+                });
         }
     },
-    computed:{
-        filteredCharacters: function(){
-            return this.marvelCharacters.filter((marvelcharacter)=>{
-                if(marvelcharacter.name[0].toLowerCase() == this.search[0]){
-                return marvelcharacter.name.toLowerCase().match(this.search.toLowerCase());
+    computed: {
+        filteredCharacters: function () {
+            return this.marvelCharacters.filter((marvelcharacter) => {
+                if (marvelcharacter.name[0].toLowerCase() == this.search[0]) {
+                    return marvelcharacter.name.toLowerCase().match(this.search.toLowerCase());
                 }
-                else if (this.search == ""){
+                else if (this.search == "") {
                     return marvelcharacter.name
                 }
-                else{
+                else {
                     return ""
                 }
             })
         }
     }
 }
-    
+
 </script>
 
 <style lang="css">
-.parent ul{
+.parent ul {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     gap: 10px;
     color: white;
 }
-button{
+
+button {
     background: white;
     color: black;
     border: 1px black solid;
     padding: 10px;
     border-radius: 20px;
 }
-img{
+
+img {
     margin: 10px;
 }
-@media (max-width: 767px){
-    .parent ul{
-    display: grid;
-    grid-template-columns: 1fr;
-    gap: 10px;
-    color: white;
+
+@media (max-width: 767px) {
+    .parent ul {
+        display: grid;
+        grid-template-columns: 1fr;
+        gap: 10px;
+        color: white;
+    }
 }
-}
-@media (min-width:768px)and (max-width: 1024px){
-    .parent ul{
+
+@media (min-width:768px)and (max-width: 1024px) {
+    .parent ul {
         display: grid;
         grid-template-columns: 1fr 1fr;
     }
--- src/components/marvel-comics.vue
+++ src/components/marvel-comics.vue
@@ -1,33 +1,33 @@
 <template lang="html">
     <div>
-        
+
         <h3>Welcome to Marvel Comics</h3>
         <!-- <div v-if="isInfoPopupVisible"><marvelCom/></div>
         <div v-else> -->
-            <input type="text" v-model="search" placeholder="Type marvel comic">
+        <input type="text" v-model="search" placeholder="Type marvel comic">
         <hr>
-            <ul>
-            <li v-for="marvelComic in filteredComics" :key="marvelComic.id" >
-            <a href="#"  type="button" ><img src="../assets/comic.jpg" alt="">
-                {{ marvelComic.title }}<button :key="marvelComic.id"  @click="() => {TogglePopup('buttonTrigger')}">Show</button>
-                
-                <Popup 
-                v-if="popupTriggers.buttonTrigger"
-                :TogglePopup="() => TogglePopup('buttonTrigger')">
-            <p>{{ marvelComic.description }}</p>
-            <p>{{ }}</p>
-            <img :src="url" alt="">
-            </Popup>
-            </a>
+        <ul>
+            <li v-for="marvelComic in filteredComics" :key="marvelComic.id">
+                <a href="#" type="button"><img :src="`${marvelComic.thumbnail.path}.${marvelComic.thumbnail.extension}`"
+                        :alt="marvelComic.title" class="comic-img" />
+                    {{ marvelComic.title }}<button :key="marvelComic.id"
+                        @click="() => { TogglePopup('buttonTrigger') }">Show</button>
+
+                    <Popup v-if="popupTriggers.buttonTrigger" :TogglePopup="() => TogglePopup('buttonTrigger')">
+                        <p>{{ marvelComic.description }}</p>
+                        <p>{{ }}</p>
+                        <img :src="url" alt="">
+                    </Popup>
+                </a>
             </li>
         </ul>
-		
-           
-            
-       
-        
-    <!-- </div> -->
-</div>
+
+
+
+
+
+        <!-- </div> -->
+    </div>
 </template>
 
 <script>
@@ -35,7 +35,7 @@ import { public_key } from '@/views/marvelCharacters';
 import { ref } from 'vue';
 import Popup from '@/components/Popup';
 // import marvelCom from '@/components/marvel-comic.vue'
-export default{
+export default {
     name: 'marvel-comics',
     components: {
         // marvelCom
@@ -53,69 +53,69 @@ export default{
         this.getMarvelComics();
         this.getMarvelComic(this.marvelComics.id)
     },
-    methods:{
-        setId(id){
+    methods: {
+        setId(id) {
             return this.marvelComics.id = id
         },
-        getId(){
+        getId() {
             return this.marvelComics.id
         },
-        getMarvelComics: function(){
+        getMarvelComics: function () {
             fetch(`http://gateway.marvel.com/v1/public/comics?apikey=${public_key}`)
                 .then(response => response.json())
                 .then((result) => {
-                console.log(result);
-                result.data.results.forEach((item) => {
-                    console.log(item);
-                    this.marvelComics.push(item);
-                  
-                });
-            })
+                    console.log(result);
+                    result.data.results.forEach((item) => {
+                        console.log(item);
+                        this.marvelComics.push(item);
+
+                    });
+                })
                 .catch((error) => {
-                console.log(error);
-            });
+                    console.log(error);
+                });
         },
         getMarvelComic: function (id) {
-           
+
             fetch(`http://gateway.marvel.com/v1/public/comics/${id}?apikey=${public_key}`)
                 .then(response => response.json())
                 .then((result) => {
-                result.data.results.forEach((item)=>{
-                    this.marvelComic.push(item)
-                    this.url = `${item.thumbnail.path}/${this.size}`
-                    console.log(this.url);
+                    result.data.results.forEach((item) => {
+                        this.marvelComic.push(item)
+                        this.url = `${item.thumbnail.path}/${this.size}`
+                        console.log(this.url);
 
+                    })
                 })
-            })
                 .catch((error) => {
-                console.log(error);
-            });
+                    console.log(error);
+                });
         }
     },
-    setup () {
-		const popupTriggers = ref({
-			buttonTrigger: false,
-		});
-		const TogglePopup = (trigger) => {
-			popupTriggers.value[trigger] = !popupTriggers.value[trigger]
-		}
-		
-		return {
-			Popup,
-			popupTriggers,
-			TogglePopup
-		}
-	},
-    computed:{
-        filteredComics: function(){
-            return this.marvelComics.filter((marvelComic)=>{
-                if(marvelComic.title[0].toLowerCase() == this.search[0]){
-                return marvelComic.title.toLowerCase().match(this.search.toLowerCase());
+    setup() {
+        const popupTriggers = ref({
+            buttonTrigger: false,
+        });
+        const TogglePopup = (trigger) => {
+            popupTriggers.value[trigger] = !popupTriggers.value[trigger]
+        }
+
+        return {
+            Popup,
+            popupTriggers,
+            TogglePopup
+        }
+    },
+    computed: {
+        filteredComics: function () {
+            return this.marvelComics.filter((marvelComic) => {
+                if (marvelComic.title[0].toLowerCase() == this.search[0]) {
+                    return marvelComic.title.toLowerCase().match(this.search.toLowerCase());
                 }
-                else if (this.search == ""){
+                else if (this.search == "") {
                     return marvelComic.title
                 }
-                else{
+                else {
                     return ""
                 }
             })
@@ -126,45 +126,57 @@ export default{
 </script>
 <style lang="css">
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	color: #2c3e50;
-	margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    margin-top: 60px;
 }
-h3{
+
+h3 {
     margin: 20px;
 }
-Popup{
+
+Popup {
     background: #000;
 }
-ul  {
+
+ul {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
 }
-ul li a{
+
+ul li a {
     font-size: medium;
     text-align: center;
 }
-button{
+
+button {
     padding: 10px;
     margin: 10px;
     font-size: 20px;
 }
-button:hover{
+
+button:hover {
     background: red;
     color: white;
-    border-radius:  1px solid white;
+    border-radius: 1px solid white;
 }
-@media (max-width: 767px){
-    ul{
+.comic-img {
+    width: 300px;
+    height: 300px;
+    object-fit: contain;
+}
+
+@media (max-width: 767px) {
+    ul {
         display: grid;
         grid-template-columns: 1fr;
     }
 }
-@media (min-width:768px)and (max-width: 1024px){
-    ul{
+
+@media (min-width:768px)and (max-width: 1024px) {
+    ul {
         display: grid;
         grid-template-columns: 1fr 1fr;
     }
 }
-
 </style>
